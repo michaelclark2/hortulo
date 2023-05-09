1bd858cbfb694eb7f1bc8602c3bb4691e5eb3738
Michael Clark <michael.clark1992@gmail.com>
2023-05-08 21:58:14 -0500

    removed unneeded config

--- hortulo/src/app/App.jsx
+++ hortulo/src/app/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import "./App.css";
 import { Heading } from "react-bulma-components";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <Heading>Hortulo</Heading>
--- hortulo/src/main.jsx
+++ hortulo/src/main.jsx
@@ -4,8 +4,19 @@ import App from "./app/App.jsx";
 import "./index.css";
 import "bulma/css/bulma.min.css";
 
+import { CeloProvider } from "@celo/react-celo";
+import "@celo/react-celo/lib/styles.css";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <CeloProvider
+      dapp={{
+        name: "Hortulo",
+        description: "Carbon offsets for mere mortals",
+        url: "https://hortulo.world",
+      }}
+    >
+      <App />
+    </CeloProvider>
   </React.StrictMode>
 );
