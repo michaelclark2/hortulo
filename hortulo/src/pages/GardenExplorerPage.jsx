9595f2da44a6f996d20126412b9cd265dc9215f0
Michael Clark <michael.clark1992@gmail.com>
2023-05-11 03:09:50 -0500

    added form to garden explorer page

--- hortulo/src/pages/GardenExplorerPage.jsx
+++ hortulo/src/pages/GardenExplorerPage.jsx
@@ -1,12 +1,46 @@
-import { Heading } from "react-bulma-components";
+import {
+  Hero,
+  Form,
+  Columns,
+  Container,
+  Button,
+  Content,
+  Heading,
+} from "react-bulma-components";
 import Layout from "../components/Layout";
 
 const GardenExplorerPage = (props) => {
   return (
     <Layout>
-      <div className="GardenExplorerPage">
-        <Heading>Explorer</Heading>
-      </div>
+      <Hero size={"fullheight"}>
+        <Hero.Header></Hero.Header>
+        <Hero.Body>
+          <Container>
+            <Columns justifyContent="center">
+              <Columns.Column size={"half"}>
+                <Form.Field>
+                  <Form.Field.Body justifyContent="center">
+                    <Form.Control textAlign={"center"}>
+                      <Form.Label>Search by Address</Form.Label>
+                      <Form.Input
+                        size={"large"}
+                        placeholder="0x0000...0000"
+                        textAlign={"center"}
+                      />
+                    </Form.Control>
+                    <Form.Control>
+                      <Button color={"danger"} style={{ width: "100%" }}>
+                        Search
+                      </Button>
+                    </Form.Control>
+                  </Form.Field.Body>
+                </Form.Field>
+              </Columns.Column>
+            </Columns>
+          </Container>
+        </Hero.Body>
+        <Hero.Footer></Hero.Footer>
+      </Hero>
     </Layout>
   );
 };
