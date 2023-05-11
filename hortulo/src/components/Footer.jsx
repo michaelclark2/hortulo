683d6b4efd4827040a363accfc2241c0e5eabcb6
Michael Clark <michael.clark1992@gmail.com>
2023-05-10 23:13:31 -0500

    added background image

--- hortulo/src/components/Footer.jsx
+++ hortulo/src/components/Footer.jsx
@@ -6,7 +6,9 @@ const Footer = (props) => {
       renderAs="footer"
       className="has-text-centered is-justify-content-center"
     >
-      <Navbar.Item textAlign={"center"}>&copy; Michael Clark, 2023</Navbar.Item>
+      <Navbar.Item renderAs={"p"} textAlign={"center"}>
+        &copy; Michael Clark, 2023
+      </Navbar.Item>
     </Navbar>
   );
 };
